3ac552cf864d2bde7b4fec309f2c09d7c4d35927
F1sher-Man <fischbach.karol96@gmail.com>
2020-03-01 21:33:44 +0100

    Data received and parsed. Diplaying companies without expand feature.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,60 @@
-import './styles/styles.scss';
+import "./styles/styles.scss";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+const _ = require("lodash");
+//TODO:
+// 1. Dodać ikonę plusa
+// 2. Przekminić jak wyświetlać zagnieżdżoną tabelę
 
 window.onload = () => {
-  const heading = document.querySelector('.heading');
-  heading.textContent = 'It\'s working!';
+  getAPIAsync("users").then(users => {
+    getAPIAsync("companies").then(companies => {
+      companies = parseData(companies, users);
+      loadTableWithData(companies);
+    });
+  });
 };
+
+async function getAPIAsync(param) {
+  let response = await fetch(`http://localhost:3000/${param}`);
+  let data = await response.json();
+  return data;
+}
+
+function parseData(companies, users) {
+  _.map(companies, function(company) {
+    company.users = _.filter(users, function(user) {
+      return company.uri === user.uris.company;
+    });
+    company.userCount = company.users.length;
+  });
+  companies = _.orderBy(companies, "userCount", "asc");
+  return companies;
+}
+
+function loadTableWithData(data) {
+  let tableHtml = `<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Company</th>
+      <th scope="col">Number of users</th>
+    </tr>
+  </thead>
+  <tbody>`;
+
+  let rowNo = 1;
+  _.map(data, function(d) {
+    tableHtml += `<tr>
+                    <th scope="row">${rowNo}</th>
+                    <td>${d.name}</td>
+                    <td>${d.userCount}</td>
+                  </tr>`;
+    rowNo += 1;
+  });
+
+  tableHtml += `</tbody>
+              </table>`;
+
+  var tableContener = document.getElementById("table-content");
+  tableContener.innerHTML = tableHtml;
+}
